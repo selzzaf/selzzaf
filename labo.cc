2e168897726a509ef5676baed45828296bd8b19b
selzzaf <afroukhselma@gmail.com>
2024-11-30 13:11:02 +0100

    labo.cc

--- labo.cc
+++ labo.cc
@@ -2,79 +2,74 @@
 #include <string>
 using namespace std;
 
-class Souris
-{
-    /*****************************************************
-      Compléter le code à partir d'ici
-    *******************************************************/
+class Souris {
 private:
     double poids;
     string couleur;
     unsigned int age;
     unsigned int esperance_vie;
-    bool clonee = false;
+    bool clonee;
+
 public:
-    //Constructeur
-    Souris(double p,string c, unsigned int a = 0 , unsigned int ev = 36) : poids(p), couleur(c),age(a),esperance_vie(ev)
-    {cout << "Une nouvelle souris !" << endl; }
-
-    Souris(Souris const& clone) : poids(clone.poids), couleur(clone.couleur), age(clone.age), esperance_vie(((0.8)* clone.esperance_vie))
-    {
-        cout << "Clonage d'une souris !" << endl;
-        clonee = true;
+    // Constructeur avec paramètres
+    Souris(double p, string c, unsigned int a = 0, unsigned int e_v = 36)
+        : poids(p), couleur(c), age(a), esperance_vie(e_v), clonee(false) {
+        cout << "Une nouvelle souris !" << endl;
     }
 
-    void afficher()
-    {
-        if (clonee == false)
-        {
-            cout<<"Une souris "<<couleur<<" de "<< age << " mois et pesant "<<poids<<" grammes"<<endl;
-        }
-        else
-        {
-            cout << "Une souris " << couleur << ", clonee, de " << age << " mois et pesant " << poids << " grammes" << endl;
-        }
+    // Constructeur de copie
+    Souris(const Souris& other)
+        : poids(other.poids), couleur(other.couleur), age(other.age),
+          esperance_vie(other.esperance_vie * 0.8), clonee(true) {
+        cout << "Clonage d’une souris !" << endl;
     }
-    void vieillir()
-    {
+
+    // Destructeur
+    ~Souris() {
+        cout << "Fin d’une souris..." << endl;
+    }
+
+    // Méthode d'affichage
+    void afficher() const {
+        cout << "Une souris " << couleur;
+        if (clonee) {
+            cout << ", clonee";
+        }
+        cout << " de " << age << " mois et pesant " << poids << " grammes" << endl;
+    }
+
+    // Méthode pour vieillir la souris
+    void vieillir() {
         age++;
-        if (clonee == true)
-        {
-            if (age > 0.5 * esperance_vie)
-            {
-                couleur = "verte";
-            }
+        if (clonee && age > esperance_vie / 2) {
+            couleur = "verte";  // La souris devient verte si elle a plus de la moitié de son espérance de vie.
         }
     }
-    void evolue()
-    {
-        while (age < esperance_vie)
-        {
+
+    // Méthode pour faire évoluer la souris
+    void evolue() {
+        while (age < esperance_vie) {
             vieillir();
         }
     }
+};
 
-    ~Souris() { cout << "Fin d'une souris..." << endl; }
-    /*******************************************
-     * Ne rien modifier après cette ligne.
-     *******************************************/
-
-}; // fin de la classe Souris
-
-int main()
-{
+int main() {
     Souris s1(50.0, "blanche", 2);
     Souris s2(45.0, "grise");
     Souris s3(s2);
-    // ... un tableau peut-être...
+
     s1.afficher();
     s2.afficher();
     s3.afficher();
+
     s1.evolue();
     s2.evolue();
     s3.evolue();
+
     s1.afficher();
     s2.afficher();
     s3.afficher();
+
     return 0;
 }
